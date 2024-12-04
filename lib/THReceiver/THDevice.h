1989cb082320fbc1ed9c895a51552a186b458081
dwaard <daan.de.waard@hz.nl>
2024-12-04 13:48:49 +0100

    Update baseline timeout

--- lib/THReceiver/THDevice.h
+++ lib/THReceiver/THDevice.h
@@ -46,7 +46,7 @@
       String _status = String("");
 
       const int UPDATE_TIMEOUT = 5 * 60 * 1000;
-      const int BASELINE_TIMEOUT = 10 * 60 * 1000;
+      const int BASELINE_TIMEOUT = 15 * 60 * 1000;
       const int BASELINE_SIZE = 3;
       const float BASELINE_TEMP_THRESHOLD = 0.4;
       float *_baselineTemps = new float[BASELINE_SIZE];
